5bacd1d21809c7d96551d9aed978e4898b967cdf
FrancescoBenassi <francybenna77@gmail.com>
2025-02-03 21:02:04 +0100

    feat Test: fixed

--- src/Test.java
+++ src/Test.java
@@ -7,30 +7,51 @@ public class Test {
         String s2 = "";
 
         for (int i = s1.length() - 1; i >= 0; i--) {
-            s2 = s2 + s1.charAt(i);
+            s2 += s1.charAt(i);
         }
         System.out.println(s2);
     }
 
     public static void snack4(ArrayList<String> string, int a, int b) {
+        if (a > b) {
+            throw new IllegalArgumentException("Il valore di 'a' non può essere inferiore a quello di 'b'");
+        }
+        if (a < 0 || b > string.size()) {
+            throw new IllegalArgumentException(
+                    "I valori di 'a' e 'b' non possono essere inferiori a 0 e non possono essere superiori alla lunghezza dell'array inserito "
+                            + string.size());
+        }
+
         ArrayList<String> stringCopy = new ArrayList<>();
         for (int i = a; i <= b; i++) {
-            if (a > b) {
-                throw new IllegalArgumentException("Il valore di 'a' non può essere inferiore a quello di 'b'");
-            }
-            if (a < 0 || b > string.size()) {
-                throw new IllegalArgumentException(
-                        "I valori di 'a' e 'b' non possono essere inferiori a 0 e non possono essere superiori alla lunghezza dell'array inserito "
-                                + string.size());
-            }
             stringCopy.add(string.get(i));
             System.out.println(string.get(i));
         }
     }
 
-    public static void main(String[] args) {
+    public static void snack4Array(String[] string, int a, int b) {
+        if (a > b) {
+            throw new IllegalArgumentException("Il valore di 'a' non può essere inferiore a quello di 'b'");
+        }
+        if (a < 0 || b > string.length) {
+            throw new IllegalArgumentException(
+                    "I valori di 'a' e 'b' non possono essere inferiori a 0 e non possono essere superiori alla lunghezza dell'array inserito "
+                            + string.length);
+        }
 
-        Test.snack3("ciao");
+        String[] stringCopy = new String[b - a];
+        for (int i = 0; i + a < b; i++) {
+            stringCopy[i] = string[i + a];
+            System.out.println(stringCopy[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Test.snack3("ciao");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
         ArrayList<String> string = new ArrayList<>();
         string.add("cibo");
@@ -47,7 +68,21 @@ public class Test {
         string.add("paese");
         string.add("trattore");
 
-        Test.snack4(string, 2, 10);
+        try {
+            Test.snack4(string, 2, 10);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
+        System.out.println("Seconda parte");
+
+        String[] string2 = { "cibo", "macchina", "spiaggia", "cielo", "persona", "lavoro", "ferie", "camino", "città",
+                "universo", "spider-man", "paese", "trattore" };
+
+        try {
+            Test.snack4Array(string2, 2, 10);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 }
